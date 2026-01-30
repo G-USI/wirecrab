d4cda5951f5a924781eda95ee1eeece916bddfbd
Yaroslav Petrov <yaroslav.v.petrov@gmail.com>
2026-01-31 00:15:16 +0100

    feat(kernel): add no_std support with conditional std feature

--- crates/kernel/src/lib.rs
+++ crates/kernel/src/lib.rs
@@ -1,11 +1,20 @@
+#![no_std]
+
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+
+#[cfg(feature = "std")]
+extern crate std;
+
+pub mod prelude {
+    pub use crate::utils::structs::*;
+}
+
+pub mod utils;
+
 pub mod application {}
-
 pub mod codec {}
-
 pub mod endpoint {}
-
 pub mod error {}
-
 pub mod document {}
-
 pub mod wire {}
---
+++ crates/kernel/src/utils.rs
@@ -0,0 +1,64 @@
+pub mod structs {
+    #[cfg(feature = "std")]
+    pub use std::vec::Vec;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::vec::Vec;
+
+    #[cfg(feature = "std")]
+    pub use std::boxed::Box;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::boxed::Box;
+
+    #[cfg(feature = "std")]
+    pub use std::string::String;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::string::String;
+
+    #[cfg(feature = "std")]
+    pub use std::rc::Rc;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::rc::Rc;
+
+    #[cfg(feature = "std")]
+    pub use std::sync::Arc;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::sync::Arc;
+
+    #[cfg(feature = "std")]
+    pub use std::collections::VecDeque;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::collections::VecDeque;
+
+    #[cfg(feature = "std")]
+    pub use std::collections::BTreeMap;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::collections::BTreeMap;
+
+    #[cfg(feature = "std")]
+    pub use std::collections::BTreeSet;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::collections::BTreeSet;
+
+    #[cfg(feature = "std")]
+    pub use std::collections::LinkedList;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::collections::LinkedList;
+
+    #[cfg(feature = "std")]
+    pub use std::collections::BinaryHeap;
+
+    #[cfg(not(feature = "std"))]
+    pub use alloc::collections::BinaryHeap;
+
+    pub use core::option::Option;
+    pub use core::result::Result;
+}
