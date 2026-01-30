de94383fa00c3644e1b60c237ec6ab1c41c98ee8
Yaroslav Petrov <yaroslav.v.petrov@gmail.com>
2026-01-30 22:25:24 +0100

    refactor: update crates to use workspace data

--- crates/kernel/src/lib.rs
+++ crates/kernel/src/lib.rs
@@ -1,16 +1,13 @@
 #![forbid(unsafe_code)]
 
-pub mod application;
-pub mod codec;
-pub mod endpoint;
-pub mod error;
-pub mod models;
-pub mod wire;
+pub mod application {}
 
-pub use application::{Application, ApplicationBuilder, ApplicationRuntime};
-pub use codec::{Codec, CodecFactory};
-pub use endpoint::{Consumer, Producer, RpcClient, RpcHandler, RpcServer};
-pub use error::{ApplicationError, CodecError, ConnectionError, EndpointError, Error, Result};
-pub use models::operation::{Action, Reply};
-pub use models::{Channel, Message, Operation, OperationBindings, ParameterValue};
-pub use wire::{EndpointParams, Wire, WireFactory};
+pub mod codec {}
+
+pub mod endpoint {}
+
+pub mod error {}
+
+pub mod document {}
+
+pub mod wire {}
