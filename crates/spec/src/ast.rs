2b7a9ae082a05db4dae3868716d2b4d937b19955
Yaroslav Petrov <yaroslav.v.petrov@gmail.com>
2026-02-01 13:13:39 +0100

    refactor(spec): use inline strings instead of include_str in tests
    - Replace include_str! macro with inline YAML strings
    - Compile-time file existence guarantee
    - Remove unused std::include_str import

--- crates/spec/src/ast.rs
+++ crates/spec/src/ast.rs
@@ -29,15 +29,6 @@ pub enum SpecError {
 ///
 /// * `Ok(serde_yaml::Value)` - Parsed AST structure
 /// * `Err(SpecError)` - Parsing error
-///
-/// # Examples
-///
-/// ```no_run
-/// use wirecrab_spec::ast::parse_yaml_ast;
-///
-/// let yaml = "asyncapi: 3.1.0\ninfo:\n  title: Test\n  version: 1.0.0";
-/// let ast = parse_yaml_ast(yaml).unwrap();
-/// ```
 pub fn parse_yaml_ast(yaml: &str) -> Result<serde_yaml::Value, SpecError> {
     Ok(serde_yaml::from_str(yaml)?)
 }
@@ -62,9 +53,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_simple_example() {
-        let ast =
-            parse_yaml_file("../../submodules/asyncapi-spec/examples/simple-asyncapi.yml").unwrap();
+    fn test_parse_simple_yaml() {
+        let yaml = r#"
+asyncapi: 3.1.0
+info:
+  title: Test API
+  version: 1.0.0
+"#;
+
+        let ast = parse_yaml_ast(yaml).unwrap();
 
         assert!(ast.is_mapping());
         let map = ast.as_mapping().unwrap();
@@ -78,8 +75,19 @@ mod tests {
 
     #[test]
     fn test_parse_with_components() {
-        let ast =
-            parse_yaml_file("../../submodules/asyncapi-spec/examples/anyof-asyncapi.yml").unwrap();
+        let yaml = r#"
+asyncapi: 3.1.0
+info:
+  title: Test API
+  version: 1.0.0
+components:
+  messages:
+    UserSignedUp:
+      name: UserSignedUp
+      title: User Signed Up
+"#;
+
+        let ast = parse_yaml_ast(yaml).unwrap();
         let map = ast.as_mapping().unwrap();
 
         let components = map
@@ -91,11 +99,51 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_streetlights_kafka() {
-        let ast = parse_yaml_file(
-            "../../submodules/asyncapi-spec/examples/streetlights-kafka-asyncapi.yml",
-        )
-        .unwrap();
+    fn test_parse_with_extensions() {
+        let yaml = r#"
+asyncapi: 3.1.0
+info:
+  title: Test API
+  version: 1.0.0
+  x-custom-field: custom value
+"#;
+
+        let ast = parse_yaml_ast(yaml).unwrap();
+        let map = ast.as_mapping().unwrap();
+
+        let info = map
+            .get(&serde_yaml::Value::String("info".to_string()))
+            .unwrap();
+        let info_map = info.as_mapping().unwrap();
+
+        assert!(info_map.contains_key(&serde_yaml::Value::String("x-custom-field".to_string())));
+    }
+
+    #[test]
+    fn test_parse_invalid_yaml() {
+        let invalid_yaml = "asyncapi: 3.1.0\nasyncapi: 2.0.0";
+
+        let result = parse_yaml_ast(invalid_yaml);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_parse_real_example() {
+        let yaml = r#"
+asyncapi: 3.1.0
+info:
+  title: Account Service
+  version: 1.0.0
+  description: This service is in charge of processing user signups
+channels:
+  userSignedup:
+    address: user/signedup
+    messages:
+      UserSignedUp:
+        $ref: '#/components/messages/UserSignedUp'
+"#;
+
+        let ast = parse_yaml_ast(yaml).unwrap();
         let map = ast.as_mapping().unwrap();
 
         assert_eq!(
@@ -105,24 +153,5 @@ mod tests {
         );
 
         assert!(map.contains_key(&serde_yaml::Value::String("channels".to_string())));
-    }
-
-    #[test]
-    fn test_parse_rpc_server() {
-        let ast =
-            parse_yaml_file("../../submodules/asyncapi-spec/examples/rpc-server-asyncapi.yml")
-                .unwrap();
-        let map = ast.as_mapping().unwrap();
-
-        assert!(map.contains_key(&serde_yaml::Value::String("id".to_string())));
-        assert!(map.contains_key(&serde_yaml::Value::String("channels".to_string())));
-    }
-
-    #[test]
-    fn test_parse_invalid_yaml() {
-        let invalid_yaml = "asyncapi: 3.1.0\nasyncapi: 2.0.0";
-
-        let result = parse_yaml_ast(invalid_yaml);
-        assert!(result.is_err());
     }
 }
